75b33de4a1cff7c03ac2f52741043729af8bbd7a
alifihsan04 <alifmuhammadihsan04@gmail.com>
2025-11-03 11:38:18 +0700

    pekan 5

---
+++ prakalpro_2025_B_2511531004/src/Pekan5/Tugas.java
@@ -0,0 +1,30 @@
+package Pekan5;
+
+public class Tugas {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 5; 
+		for (int line = 1; line <= n; line++) { // untuk buat jadi 5 baris 
+			// untuk buat titik dari kiri
+			for (int j = 1; j <=  (-1 * line + 5); j++) {
+				System.out.print(".");
+			
+					
+				}
+			// cetak angka
+			System.out.print(line);
+			// cetak titik dari kanan
+			for (int j = 1; j < line; j++ ) {
+				System.out.print(".");
+				
+			}
+			// untuk pindah baris 
+			System.out.println();
+		
+		}
+	}
+
+
+}
---
+++ prakalpro_2025_B_2511531004/src/Pekan5/nestedFor3.java
@@ -0,0 +1,27 @@
+package Pekan5;
+
+public class nestedFor3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 5; 
+		for (int line = 1; line <= n; line++) { // untuk buat jadi 5 baris 
+			// untuk buat titik dari kiri
+			for (int j = 1; j <=  (-1 * line + 5); j++) {
+				System.out.print(".");
+			
+					
+				}
+			// cetak angka
+			System.out.print(line);
+			
+			// untuk pindah baris 
+			System.out.println();
+		
+		}
+	}
+}
+
+
+
